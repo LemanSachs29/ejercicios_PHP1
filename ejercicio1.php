cd8e0f9b82380d46ce69aa5620e46c0d39ffdcf2
LemanSachs29 <juanblancomoyano@gmail.com>
2024-10-06 21:16:50 +0200

    Actualización

---
+++ ejercicio1.php
@@ -0,0 +1,16 @@
+<?php
+// Declare una variable llamada $nombre y asígnale tu nombre.
+$nombre = Juan;
+echo"</br>";
+//Imprima un saludo utilizando la función echo como: "Hola, mi nombre
+// es [tu nombre]".
+
+echo "Hola mi nombre es $nombre";
+//Usa comentarios para explicar cada línea de tu código.
+//Define una constante llamada PI con el valor 3.1416 y muestra su valor en
+//pantalla.
+
+define("PI", 3.1416);
+//Prueba con dos variables cuyos nombres solo difieran en mayúsculas y
+//demuestra que PHP es sensible a mayúsculas y minúsculas.
+?>
--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-//Inicio del tallerPHP
+<?php
+//1. Instala XAMPP en tu ordenador.
+//2. Inicia Apache y MySQL desde el panel de control de XAMPP.
+//3. Accede a http://localhost en tu navegador para comprobar que Apache está
+//funcionando correctamente.
+//4. Crea un archivo index.php en la carpeta htdocs de XAMPP, y escribe un script
+//simple en PHP que muestre tu nombre usando la función echo.
+//5. Accede a http://localhost/index.php para ver tu script en acción.
+//6. Ingresa a http://localhost/phpmyadmin y crea una base de datos llamada
+//"mi_base_de_datos".
+
+?>
