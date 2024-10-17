9f948450bb897552e998d0c5b4bcea43e1ab7568
LemanSachs29 <163987047+LemanSachs29@users.noreply.github.com>
2024-10-17 14:41:33 +0200

    Fin clase jueves 17/10

---
+++ EjerciciosPHP/t4ej1.php
@@ -0,0 +1,27 @@
+<?php
+/*1. Ejercicio con Operadores de Asignación y Condicionales
+Declara una variable con un valor inicial de 100. Usa operadores de asignación (
++=, -=, etc.) para modificar su valor en diferentes etapas y luego usa una
+condicional ifpara determinar si el valor final es mayor o menor que 100.. */
+?>
+
+
+<?php
+$numero = 100;
+
+echo "sumo 25 a la vble usando el operador +="
+
+$numero += 25;
+
+echo "Ahora numero vale $numero"
+
+echo "resto 25 a la vble usando el operador +="
+
+$numero -= 25;
+
+echo "Ahora numero vale $numero"
+
+if($numero == 100){
+    echo "numero sigue siendo = 100"
+}
+?>
---
+++ EjerciciosPHP/t4ej2.php
@@ -0,0 +1,28 @@
+<?php
+/* Ejercicio con Operadores de Comparación y Lógicos
+Declara tres variables: $a, $by $c, con valores numéricos diferentes. Utilice
+operadores de comparación y lógicos para verificar si $aes mayor que $b y si $ces
+menor que $a. Imprima un mensaje que indique si ambas condiciones son
+verdaderas o si alguna es falsa.*/
+?>
+
+
+<?php
+$a = 1;
+$b = 2;
+$c = 3;
+
+if($a > $b ){
+    echo "a > b"
+    
+}else{
+    echo "!(a > b)"
+    if
+}
+
+if($c < $a){
+    echo "c < a"
+}else{
+    echo "!(c < a)"
+}
+?>
