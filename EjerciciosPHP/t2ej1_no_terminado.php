ead42d8423d22f712e81ca5653f7aeada3d7067a
LemanSachs29 <juanblancomoyano@gmail.com>
2024-10-20 14:00:48 +0200

    Create t2ej1_no_terminado.php

---
+++ EjerciciosPHP/t2ej1_no_terminado.php
@@ -0,0 +1,19 @@
+<?php
+
+/**Ejercicio con echo y HTML Embebido
+Crea un archivo PHP que imprime un formulario HTML usando la función echo. El
+formulario debe contener un campo de texto para el nombre y un botón de envío.
+Cuando el formulario se envía, muestra el nombre ingresado utilizando la misma
+página PHP. */
+
+echo "<label for="fname">Nombre:</label><br>"
+echo "<input type="text" id="fname" name="fname" value=""><br>"
+
+
+//Sin terminar
+
+
+
+
+
+?>
